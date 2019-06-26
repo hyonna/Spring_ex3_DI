eaf11dcf29fea4208a98ad10c0a73182f096ced4
hyonna <yha0118@naver.com>
2019-06-26 12:10:07 +0900

    Initial commit

--- src/main/java/com/iu/s3/HomeController.java
+++ src/main/java/com/iu/s3/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.iu.Car.Wheel;
 
 /**
  * Handles requests for the application home page.
@@ -22,10 +26,15 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	@Inject
+	private Wheel wheel;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		
-		
+		System.out.println(wheel.getSize());
+		System.out.println(wheel.getBrand());
 		
 		return "home";
 	}
